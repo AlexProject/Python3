4e37f03ad7344ff12216a2056735ff1f31c2b050
AlexProject <psekha@gmail.com>
2017-07-24 13:28:38 +0300

    test

---
+++ function.py
@@ -0,0 +1,17 @@
+def pr(a, b):
+    if a > b:
+        print(a, 'max')
+    elif a == b:
+        print(a, "the same as", b)
+    else:
+        print(a, "less")
+
+pr(10, 9)
+
+x = 5
+y = 6
+pr(x,y)
+
+
+
+
